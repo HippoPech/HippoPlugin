64d641ed5d3d14ba91463b3fb2f5387b3334930b
Inposa <donhippo04@gmail.com>
2026-01-31 15:33:26 +0100

    Add HelloWorld test command

--- src/main/java/fr/hippo/HippoPlugin.java
+++ src/main/java/fr/hippo/HippoPlugin.java
@@ -1,22 +1,27 @@
-package dev.hytalemodding;
+package fr.hippo;
 
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.Interaction;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import dev.hytalemodding.commands.ExampleCommand;
 import dev.hytalemodding.events.ExampleEvent;
+import fr.hippo.commands.HelloWorld;
+import fr.hippo.interactions.ArtifactPushback;
 
 import javax.annotation.Nonnull;
 
-public class ExamplePlugin extends JavaPlugin {
+public class HippoPlugin extends JavaPlugin {
 
-    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+    public HippoPlugin(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
     @Override
     protected void setup() {
-        this.getCommandRegistry().registerCommand(new ExampleCommand("example", "An example command"));
-        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ExampleEvent::onPlayerReady);
+        //this.getCommandRegistry().registerCommand(new ExampleCommand("example", "An example command"));
+        //this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ExampleEvent::onPlayerReady);
+        this.getCodecRegistry(Interaction.CODEC).register("artifact_pushback", ArtifactPushback.class, ArtifactPushback.CODEC);
+        this.getCommandRegistry().registerCommand(new HelloWorld("hello", "A simple Hello World command"));
     }
 }
