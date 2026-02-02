89c0a6f66ddd1f146938a8d84599d2cfed99e22a
Inposa <donhippo04@gmail.com>
2026-02-03 00:03:16 +0100

    Clean velocity code with ECS HeadRotation Component

--- src/main/java/fr/hippo/commands/Velocity.java
+++ src/main/java/fr/hippo/commands/Velocity.java
@@ -2,15 +2,13 @@ package fr.hippo.commands;
 
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.math.util.TrigMathUtil;
-import com.hypixel.hytale.math.vector.Transform;
 import com.hypixel.hytale.math.vector.Vector3d;
-import com.hypixel.hytale.math.vector.Vector3f;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.arguments.system.DefaultArg;
 import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.entity.knockback.KnockbackComponent;
+import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
@@ -29,25 +27,10 @@ public class Velocity extends AbstractPlayerCommand {
     protected void execute(@NonNullDecl CommandContext commandContext, @NonNullDecl Store<EntityStore> store, @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world) {
         if (playerRef.getReference() == null) return;
         float velocityForce = (this.force.get(commandContext)) * 10;
-        Vector3f headRotation = playerRef.getHeadRotation();
-
-        Vector3d forward = (getForwardVector(headRotation)).scale(velocityForce);
-
+        HeadRotation headRotation = store.getComponent(ref, HeadRotation.getComponentType());
+        Vector3d forward = headRotation.getDirection().scale(velocityForce);
         KnockbackComponent knockback = new KnockbackComponent();
         knockback.setVelocity(forward);
         store.addComponent(playerRef.getReference(), KnockbackComponent.getComponentType(), knockback);
-    }
-
-    private Vector3d getForwardVector (Vector3f headRotation){
-        float forwardX = TrigMathUtil.cos(headRotation.getPitch());
-        float forwardY = TrigMathUtil.sin(headRotation.getPitch());
-        float forwardZ = TrigMathUtil.cos(headRotation.getPitch())  * TrigMathUtil.cos(headRotation.getYaw());
-
-        return (new Vector3d(forwardX,forwardY,forwardZ));
-    }
-    private Vector3d getBackwardVector (Vector3f headRotation){
-        Vector3d vector =  getForwardVector(headRotation);
-        vector.setY(-headRotation.y);
-        return vector;
     }
 }
